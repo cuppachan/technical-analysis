01dcde455b4f5f4456baf3b376cbce0af328f560
cuppachan <cuppachan@gmail.com>
2014-08-13 19:54:10 +0100

    TechnicalAnalysis Part 4

--- technical_analysis.py
+++ technical_analysis.py
@@ -6,10 +6,24 @@ stocksToPull ='AAPL','GOOG','MSFT','CMG','AMZN','EBAY','TSLA'
 
 def pullData(stock):
 	try:
-					
-		
+  
+		print 'Currently Pullingstock',stock
+		print str(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d' %H:%M:%S'))
+		urlToVisit = 'http://ichart.finance.yahoo.com/table.csv?s='+stock
+		saveFileLine = stock+'.txt'
+                
+		try:
+			readExistingData = open(saveFileLIne,'r').read()
+			splitExisting = readExistingData.split('\n')
+			mostRecentLine = splitExisting(-2)
+			lastUnix = int(mostRecentLine.split(',')[0])
+		except: 
+			lastUnix = 0 
+
+	
+	
 	except Exception,e:
 		print 'main loop',str(e)
-		
-for eachStock in stocksToPull:
-	pullData(eachStock)
+
+	for eachStock in stocksToPull:
+		pullData(eachStock)
